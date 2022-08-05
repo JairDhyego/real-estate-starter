735912e98ee3696b1f99bc7e5eadb1f81c903c3d
Jair Dhyego <jaiirdhyego@gmail.com>
2022-08-05 04:09:14 -0300

    commit final

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# real-estate-starter
+
+# TITLE
+Projeto de criação de sistema de venda de imovel.
+
+## texto
+Fala meu pvo, boa noite, eu achei um canal com otímos locais para aprender esse tipo de projeto, eu já fiz vários e esse aqui é mais o link do vídeo no youtube é :https://www.youtube.com/watch?v=CHe_QJcTK5Y&t=5499s 
+
+
+## finalizado.
+
+Concluído.
+
+## Link para o projeto:
+
+## tecnologias.
+Feito com framework react e usando a linguagem java script, e tailwind nesse projeto.
+
+## Codigo para rodar:
+npm start.
--- src/components/HouseList.js
+++ src/components/HouseList.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useContext } from "react";
+
+import { HouseContext } from "./HouseContext";
+
+import House from "./House";
+
+import { Link } from "react-router-dom";
+
+import { ImSpinner2 } from "react-icons/im";
 
 const HouseList = () => {
-  return <div>HouseList</div>;
+  const { houses, loading } = useContext(HouseContext);
+
+  if (loading) {
+    return (
+      <ImSpinner2 className="mx-auto animate-spin text-violet-700 text-4xl lg:mt-[400px]" />
+    );
+  }
+
+  if (houses.length < 1) {
+    return (
+      <div className="text-center text-3xl text-gray-400 ">
+        Sorry, nothing found!
+      </div>
+    );
+  }
+
+  return (
+    <section className="mb-20">
+      <div className="container mx-auto">
+        <div className="grid md:grid-cols-2 lg:pt-48 gap-4 lg:grid-cols-3">
+          {houses.map((house, i) => {
+            return (
+              <Link to={`/property/${house.id}`} key={i}>
+                <House house={house} />
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HouseList;
